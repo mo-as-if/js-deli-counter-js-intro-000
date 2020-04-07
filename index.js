f013864b6f1289a50ac7df7ec0fe53951e688763
Mohaimeen Hossain <mhossain5464@gmail.com>
2020-04-07 15:55:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeliLine = []; // empty array to be filled
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name); // pushes new name to end of array.
+  var index = katzDeliLine.length
+  return `Welcome, ${name}. You are number ${index} in line.`
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length > 0) {
+    return `Currently serving ${katzDeliLine.shift()}.` // shift() removes first element of array
+  } else {
+    return "There is nobody waiting to be served!" // aka katzDeliLine.length <= 0
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    var lineup = [] // empty string to build new array.
+    for (var i = 0; i < katzDeliLine.length; i++) {  //increments index
+      lineup += (i + 1) + ". " + katzDeliLine[i] + ", "; // 1. Bill, 2. Jane,
+    }
+    lineup = lineup.slice(0, -2);
+    return `The line is currently: ${lineup}`;
+  } else {
+    return "The line is currently empty."
+  }
+}
